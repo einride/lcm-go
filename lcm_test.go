6f04b095daf822858d09a794e05f1ab6c8474161
Oscar Söderlund <odsod@users.noreply.github.com>
2018-11-06 12:45:10 +0100

    Fix bug, sequence number not incremented

--- lcm_test.go
+++ lcm_test.go
@@ -1,6 +1,7 @@
 package lcm_test
 
 import (
+	"encoding/binary"
 	"net"
 	"testing"
 
@@ -30,4 +31,26 @@ func TestLCM_Publish(t *testing.T) {
 	expected = append(expected, 0)                    // null string terminator
 	expected = append(expected, []byte("payload")...) // payload
 	require.Equal(t, expected, data[:n])
+}
+
+func TestLCM_Publish_Multiple(t *testing.T) {
+	n := 100
+	addr, err := net.ResolveUDPAddr("udp", "239.255.76.67:7667")
+	require.NoError(t, err)
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	require.NoError(t, err)
+	go func() {
+		lc, err := lcm.Create("udpm://239.255.76.67:7667")
+		require.NoError(t, err)
+		for i := 0; i < n; i++ {
+			require.NoError(t, lc.Publish("channel", []byte("payload")))
+		}
+		require.NoError(t, lc.Close())
+	}()
+	data := make([]byte, 1024)
+	for i := 0; i < n; i++ {
+		_, _, err := conn.ReadFromUDP(data)
+		require.NoError(t, err)
+		require.Equal(t, uint32(i), binary.BigEndian.Uint32(data[4:]))
+	}
 }
