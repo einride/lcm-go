ff545250ad1fc124a9bc5b3c819501fed02fcc0d
Magnus Röös <magnus.roos@einride.tech>
2019-10-05 15:14:40 +0200

    do more than one byte at a time if we can

--- bpf.go
+++ bpf.go
@@ -1,15 +1,14 @@
 package lcm
 
 import (
+	"encoding/binary"
+
 	"github.com/golang/protobuf/proto"
 	"golang.org/x/net/bpf"
 )
 
 // indexOfUDPPayload is the first byte index of the payload in a a UDP packet.
-const (
-	indexOfUDPPayload          = 8
-	jumpNextChannelPlaceholder = 254
-)
+const indexOfUDPPayload = 8
 
 // ShortMessageFilter accepts only LCM short messages.
 func ShortMessageFilter() []bpf.Instruction {
@@ -23,6 +22,7 @@ func ShortMessageFilter() []bpf.Instruction {
 
 // ShortMessageChannelFilter accepts LCM short messages where the channel equals any of the specified channels.
 func ShortMessageChannelFilter(channels ...string) []bpf.Instruction {
+	const jumpNextChannelPlaceholder = 254
 	const estimatedInstructionsPerChannel = 30
 	program := make([]bpf.Instruction, 0, estimatedInstructionsPerChannel*len(channels))
 	// accept only short messages
@@ -33,38 +33,53 @@ func ShortMessageChannelFilter(channels ...string) []bpf.Instruction {
 	)
 	// check for each channel, accept if any matches
 	for _, channel := range channels {
-		for i := 0; i < len(channel); i++ {
-			// check if the i:th byte matches, skip to next channel if not
+		channel := []byte(channel)
+		for i, size := 0, 0; i < len(channel); i += size {
+			remaining := channel[i:]
+			var val uint32
+			switch len(remaining) {
+			case 1:
+				val = uint32(remaining[0])
+				size = 1
+			case 2, 3:
+				val = uint32(binary.BigEndian.Uint16(remaining))
+				size = 2
+			default:
+				val = binary.BigEndian.Uint32(remaining)
+				size = 4
+			}
 			currByteIndex := indexOfUDPPayload + indexOfChannel + uint32(i)
 			program = append(program,
-				bpf.LoadAbsolute{Off: currByteIndex, Size: 1},
-				bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: uint32(channel[i]), SkipTrue: jumpNextChannelPlaceholder},
+				bpf.LoadAbsolute{Off: currByteIndex, Size: size},
+				bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: val, SkipTrue: jumpNextChannelPlaceholder},
 			)
 		}
 		byteIndex := indexOfUDPPayload + indexOfChannel + uint32(len(channel))
 		program = append(program,
 			bpf.LoadAbsolute{Off: byteIndex, Size: 1},
-			// If there is a query parameter accept the message as is
+			// If there is a query parameter accept the message as is.
 			bpf.JumpIf{Cond: bpf.JumpEqual, Val: '?', SkipTrue: 1},
 			bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: 0, SkipTrue: 1},
-			// channel match, accept package
+			// Channel match, accept package.
 			bpf.RetConstant{Val: lengthOfLargestUDPMessage},
 		)
 	}
-	// no channel match, reject package
+	// No channel match, reject package.
 	program = append(program, bpf.RetConstant{Val: 0})
-	// fill in the missing instruction
+	// Start with next channel pointing to the rejection we just added
 	nextChannelPos := uint8(len(program)) - 1
+	// Now we back-track through the program to find the placeholders and
+	// rewrite those to a real jump to the next channel (or the reject instructino).
 	for i := uint8(len(program)) - 1; i >= 3; i-- {
 		switch instr := program[i].(type) {
 		case bpf.JumpIf:
 			if instr.SkipTrue != jumpNextChannelPlaceholder {
 				continue
 			}
-			instr.SkipTrue = nextChannelPos - i - 1 // remove one, since the skip is "off by one"
+			instr.SkipTrue = nextChannelPos - i - 1 // Remove one, since the skip is "off by one".
 			program[i] = instr
 		case bpf.LoadAbsolute:
-			// Each channel starts with a load of the first byte in channel
+			// Each channel matching starts with a load of the first byte in channel.
 			if instr.Off != indexOfUDPPayload+indexOfChannel {
 				continue
 			}
