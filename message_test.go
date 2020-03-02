730bf3ba8506a2fcaa13c00c271b7a9255b5edba
Magnus Röös <magnus.roos@einride.tech>
2020-03-02 08:14:35 +0100

    add test for param splitting

--- message_test.go
+++ message_test.go
@@ -39,6 +39,21 @@ func TestMessage_MarshalUnmarshal(t *testing.T) {
 				Data:           []byte{0x01, 0x02, 0x03},
 			},
 		},
+		{
+			msg: "payload channel with params",
+			data: []byte{
+				0x4c, 0x43, 0x30, 0x32, // short header magic
+				0x12, 0x34, 0x56, 0x78, // sequence number
+				'a', 'b', 'c', '?', 'z', '=', 'l', 'z', '4', 0x00, // channel
+				0x01, 0x02, 0x03, // payload
+			},
+			message: Message{
+				SequenceNumber: 0x12345678,
+				Channel:        "abc",
+				Params:         "z=lz4",
+				Data:           []byte{0x01, 0x02, 0x03},
+			},
+		},
 	} {
 		tt := tt
 		t.Run(tt.msg, func(t *testing.T) {
